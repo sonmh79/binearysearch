e6576275ba39de38f0f661b3775318061b4c9489
sonmh79 <78152114+sonmh79@users.noreply.github.com>
2021-02-17 23:30:05 +0900

    Recursion/bisect module

---
+++ 704. Binary Search.py
@@ -0,0 +1,26 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        # 1. 이진 검색 재귀 풀이
+        def binarysearch(left,right):
+            if left <= right:
+                mid = (left+right) // 2
+                
+                if nums[mid] > target:
+                    return binarysearch(left,mid-1)
+                elif nums[mid] < target:
+                    return binarysearch(mid+1,right)
+                else:
+                    return mid
+            else:
+                return -1   
+        return binarysearch(0,len(nums)-1)
+        
+        # 2. bisect 이진 검색 모듈
+            index = bisect.bisect_left(nums,target)
+            if index < len(nums) and nums[index] == target:
+                return index
+            else:
+                return -1
+            
+                    
+                
